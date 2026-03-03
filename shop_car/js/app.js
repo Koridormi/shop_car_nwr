660da4a47416cf29757fa7ea3bec440b5a22974c
Koridormi <258958198+Koridormi@users.noreply.github.com>
2026-03-03 20:14:03 -0300

    Proyecto Finalizado

--- shop_car/js/app.js
+++ shop_car/js/app.js
@@ -7,7 +7,17 @@ let articulos = [];
 
 registarEventListeners();
 function registarEventListeners() {
+    // event de click
     productos.addEventListener('click', agregarProducto);
+
+    // eliminar productos
+    carrito.addEventListener('click', eliminarProducto);
+
+    // vaciar carrito
+    cleanshop.addEventListener('click',() => {
+        articulos = []; // se resetea el array
+        limpiarHTML();
+    });
 };
 
 // function para seleccionar el contenido correcto
@@ -18,6 +28,14 @@ function agregarProducto(e) {
         leerDatosProducto(productoSeleccionado)
     }
 };
+// elimina un producto del carrito
+function eliminarProducto(e) {
+    if(e.target.classList.contains('borrar-curso')) {
+        const productoId = e.target.getAttribute('data-id');
+        articulos = articulos.filter((producto) => producto.id !== productoId);
+        carritoHTML(); // iterar sobre el carrito y mostrar su HTML...
+    }
+}
 
 // lee el contenido HTML al que dimos click y extrae su informacion
 function leerDatosProducto(producto) {
@@ -28,9 +46,25 @@ function leerDatosProducto(producto) {
         id : producto.querySelector('a').getAttribute('data-id'),
         cantidad : 1
     }
-    // agrega los elementos al array de articulos
-    articulos = [...articulos, infoProducto];
 
+    // revisa si el producto ya existe dentro del carrito de compras
+    const existe = articulos.some((producto) => producto.id === infoProducto.id);
+    if(existe) {
+        const repetido = articulos.map((producto) => {
+            if(producto.id === infoProducto.id) {
+                producto.cantidad++;
+                return producto; // retorna objetos duplicados
+            } else {
+                return producto; // retorna objetos no duplicados
+            }
+        });
+        articulos = [...repetido];
+    } else {
+        // agrega los elementos al array de articulos
+        articulos = [...articulos, infoProducto];
+    };
+
+    // esta funciona muestra los articulos en el HTML
     carritoHTML();
 }
 
@@ -42,10 +76,23 @@ function carritoHTML() {
 
     // recorre el carrito y genera el HTML
     articulos.forEach((producto) => {
+        const {imagen,nombre,precio,id,cantidad} = producto; // disctrocturing (???
         const row = document.createElement('tr');
         row.innerHTML = `
             <td>
-                ${producto.nombre}
+                <img src='${imagen}' width=100>
+            </td>
+            <td>
+                ${nombre}
+            </td>
+            <td>
+                ${precio}
+            </td>
+            <td>
+                ${cantidad}
+            </td>
+            <td>
+                <a href='#' class='borrar-curso' data-id='${id}'> X </a>
             </td>
         `;
 
